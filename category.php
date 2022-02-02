8de6b93e75d7ec6402ed655c6d36988ce34bc736
WnX <vandermeulen.christophe@gmail.com>
2022-02-02 20:52:55 +0100

    Création categorie

---
+++ category.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="category_save.php" method="POST">
+        <div>
+            <label>Name</label>
+            <input type="text" name="nom" value="" placeholder="Place ici votre nom">
+        </div>
+        <div>
+            <input type="submit" value="Envoyer">
+        </div>
+    </form>
+</body>
+</html>
