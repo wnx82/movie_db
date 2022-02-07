77b57c0995fb8cb778f9b2aec21d3519acaf9aa8
WnX <vandermeulen.christophe@gmail.com>
2022-02-07 20:42:24 +0100

    maj connexion

--- connexion.php
+++ connexion.php
@@ -1,2 +1,17 @@
 <?php
+//phpinfo();
 
+// PDO  https://www.php.net/manual/en/pdo.connections.php
+
+use JetBrains\PhpStorm\Internal\PhpStormStubsElementAvailable;
+
+$user= 'eleve';
+$pass= 'eleve';
+
+try {
+    $dbh = new PDO('mysql:host=localhost;port=3307;dbname=movie_db', $user, $pass);
+    echo 'ça marche';
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
