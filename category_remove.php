7cb266c8f76d3db4973db11f997c6f058373666d
WnX <vandermeulen.christophe@gmail.com>
2022-03-07 21:07:49 +0100

    update

--- category_remove.php
+++ category_remove.php
@@ -2,7 +2,7 @@
 // connexion à la base
 require_once ('connexion.php');
 
-
+$id = $_GET['id'] ?? null;
 
 //si l'id est null => liste
 if($id == null){
@@ -11,10 +11,10 @@ if($id == null){
 //forcer le type
 $id = (int) $id;
 //$id=isset($_get['id']) ? $_GET['id'] : null;
-$id=$_GET['id']  ?? null;
-//code supprimer
-$query = $db->query ('delete from category where id='.$id);
+// $id=$_GET['id']  ?? null;
 
+//code supprimer
+$query = $db->query('delete from category where id=' . $id);
 //si il y a une erreur
 if($query == false){
     exit('Une erreur s\'est produite, veuillez réessayer plus tard !');
--- category_save.php
+++ category_save.php
@@ -1,16 +1,25 @@
 <?php
-require_once ('connexion.php');
+require_once('connexion.php');
 
 //echo get_class($db),exit;
 
-if (!isset($_POST['name']) || empty($_POST['name'])){
+$id = $_GET['id'] ?? null;
+
+if (!isset($_POST['name']) || empty($_POST['name'])) {
     echo 'le nom est obligatoire';
     exit;
 }
-$name=$_POST['name'];
+$name = $_POST['name'];
+
+if ($id != null) {
+    $query = $db->query("update category set name='$name' where id=$id");
+} else {
+    $query = $db->query('insert into category (name) values ("' . $name . '")');
+}
 
 
-$result = $db->query('insert into category (name) values ("'.$name.'")');
+header('location:category_list.php');
+
 
 
 /*var_dump($result);
