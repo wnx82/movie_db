93430278550ebb6e6eba9235c1650e6221af7ec5
WnX <vandermeulen.christophe@gmail.com>
2022-03-07 21:07:45 +0100

    Update

--- category.php
+++ category.php
@@ -1,55 +1,41 @@
 <?php
+require_once('connexion.php');
 
-//récupérer l'id si ID.
-
-//$id=isset($_get['id']) ? $_GET['id'] : null;
-$id=$_GET['id']  ?? null;
+$id = $_GET['id'] ?? null;
+$category = null;
 
 
-//forcer le type
-$id = (int) $id;
 
-// si l'id existe
-if(isset($id)){
-
-        // Récupérer la connexion BDD
-        require_once ('connexion.php');
-
-        //Requete pour récupérer la catégorie
-        $req= $db->query('select*from category where id='.$id.'')or die (print_r($db->errorInfo()));
-        while ($donnees=$req->fetch()){
-        // print_r(catégorie)
-        print_r($donnees['name']);       
-        }
-
-//       $query = $db->query ('update category set name='.$_POST['name'].'  where id='.$id);
-
- 
-    }
-
+if ($id != null) {
+    $query = $db->query('select * from category where id=' . $id);
+    $category = $query->fetch(PDO::FETCH_ASSOC);
+    $url = '?id=' . $id;
+} else {
+    $url = '';
+}
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <form action="category_save.php" method="POST">
+    <form action="category_save.php<?= $url ?>" method="post">
         <div>
             <label>Name</label>
-            <input type="text" name="name" value="<?=$donnees['name'] ?? '' ?>" placeholder="Placez ici votre nom de catégorie">
-
-
+            <input type="text" value="<?= $category['name'] ?? '' ?>" name="name" placeholder="Placez ici votre nom">
         </div>
         <div>
             <input type="submit" value="Envoyer">
         </div>
     </form>
 </body>
+
 </html>
